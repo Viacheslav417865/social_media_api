df642049160662d34a0b20d6dbf0f82b3455e4c2
Viacheslav417865 <sfinks417865@gmail.com>
2024-12-21 23:33:18 +0200

    Initial migrations for social and user apps

--- manage.py
+++ manage.py
@@ -6,7 +6,10 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "social_m.settings")
+    os.environ.setdefault(
+        "DJANGO_SETTINGS_MODULE",
+        "social_media.settings"
+    )
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
