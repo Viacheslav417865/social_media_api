31e1c5fe556f2d768122bf6113b1dfef8cf76cbf
Viacheslav417865 <sfinks417865@gmail.com>
2024-12-21 23:07:47 +0200

    Update social and user app models and admin registrations

--- social_media/settings.py
+++ social_media/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "user",
     "social",
 ]
@@ -96,6 +97,8 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+AUTH_USER_MODEL = "user.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
